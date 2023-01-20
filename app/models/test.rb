c0410b4ac1f3c6d67ef47f912e335e6cee653aff
N1ghtcr4wl33r <paveltrsv08@gmail.com>
2023-01-20 15:23:55 +0300

    add: users_tests model, test data in seeds.rb, instance method for User, class method for Test

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,3 +1,7 @@
 class Test < ApplicationRecord
   belongs_to :category
+
+  def self.by_category(category_name)
+    joins(:category).where(categories: {title:category_name}).order(title: :desc).pluck(:title)
+  end
 end
