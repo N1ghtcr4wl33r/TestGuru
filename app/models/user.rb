0eb3855c77d2678d61aaa69783ce5b15bc4e2de0
N1ghtcr4wl33r <paveltrsv08@gmail.com>
2023-01-30 23:14:42 +0300

    fixed: refactoring of instance method for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   has_many :created_tests, class_name: 'Test', foreign_key: 'author_id'
   
   def tests_by_level(level)
-    Test.joins('JOIN tests_users ON tests.id = tests_users.test_id').where('tests.level = :level AND tests_users.user_id = :user_id', level: level, user_id: id)
+    tests.where(level: level)
   end
 end
