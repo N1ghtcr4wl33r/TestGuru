a035739681857d510304d12b7b5af0d84899f018
N1ghtcr4wl33r <paveltrsv08@gmail.com>
2025-11-27 15:41:03 -0300

    add: controllers for tests and answers, helpers for tests, questions and answers, templates for tests, questions and answers, footer and errors partials; refactoring in applications and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def year_current
+    Date.current.year
+  end
+
+  def github_url(author, repo)
+    link_to 'Trusov Pavel, TestGuru', "https://github.com/#{author}/#{repo}/", target: '_blank'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  root 'tests#index'
+
   resources :tests do
     resources :questions, shallow: true
   end
+
+  resources :questions do
+    resources :answers, shallow: true
+  end
+
 end
